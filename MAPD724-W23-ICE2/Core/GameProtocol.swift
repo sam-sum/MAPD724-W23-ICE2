51818982abb4f877e0a2105b52c673397a6acdf4
sam-sum <113701007+sam-sum@users.noreply.github.com>
2023-01-23 22:25:09 -0500

    Initial commit

---
+++ MAPD724-W23-ICE2/Core/GameProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  GameProtocol.swift
+//  MAPD724-W23-ICE2
+//
+//  Created by Samuel Sum on 2023-01-24.
+//
+protocol GameProtocol
+{
+    func Start()
+    
+    func Update()
+    
+    func CheckBounds()
+    
+    func Reset()
+}
